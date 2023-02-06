0d35872c59fa96aa2606af3230f3699f6d03f78e
akrrkdgksdywjd <106609216+akrrkdgksdywjd@users.noreply.github.com>
2023-02-06 14:25:04 +0900

    박채원 xml 파일

---
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Store_shoppingbasket.kt
@@ -0,0 +1,11 @@
+package com.aiden.tflite.tfliteimageclassifier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Store_shoppingbasket : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_shoppingbasket)
+    }
+}
