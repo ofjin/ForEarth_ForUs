7539e386e744aa3928ff07f7ad074f817bb22fb2
yeon <yeon0321@swu.ac.kr>
2023-02-06 15:15:14 +0900

    메인,디테일

---
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/StoreDetail.kt
@@ -0,0 +1,11 @@
+package com.aiden.tflite.tfliteimageclassifier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_detail)
+    }
+}
