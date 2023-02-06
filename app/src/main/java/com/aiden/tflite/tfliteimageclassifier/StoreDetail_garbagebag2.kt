74bf5498b178f5e4eb70ee19741de8d81583ecf8
wlsskdbwls <wlsskdbwls@naver.com>
2023-02-06 21:03:29 +0900

    스토어 메인 & 디테일 추가

---
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/StoreDetail_garbagebag2.kt
@@ -0,0 +1,11 @@
+package com.aiden.tflite.tfliteimageclassifier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreDetail_garbagebag2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_detail_garbagebag2)
+    }
+}
--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/StoreDetail_sticker3.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/StoreDetail_sticker3.kt
@@ -3,9 +3,9 @@ package com.aiden.tflite.tfliteimageclassifier
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class StoreDetail : AppCompatActivity() {
+class StoreDetail_sticker3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_store_detail)
+        setContentView(R.layout.activity_store_detail_sticker3)
     }
 }
