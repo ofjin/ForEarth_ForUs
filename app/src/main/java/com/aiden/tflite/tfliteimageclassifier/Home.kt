9af4c37a8ac6535b3224281fc21e58df006055a0
akrrkdgksdywjd <106609216+akrrkdgksdywjd@users.noreply.github.com>
2023-02-09 21:52:26 +0900

    홈화면 히스토리 수정

--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
@@ -6,11 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_home_test.*
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //히스토리로 이동
+        history_button.setOnClickListener{
+            startActivity(Intent(this,history::class.java))
+        }
 
     }
 
--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/history.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/history.kt
@@ -1,11 +1,19 @@
 package com.aiden.tflite.tfliteimageclassifier
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home_test.*
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class history : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        //홈 화면으로 돌아가기
+        back_myinfo.setOnClickListener{
+            startActivity(Intent(this,home::class.java))
+        }
     }
 }
