702b67780b6749f27d1d75336461ca37002f30fc
akrrkdgksdywjd <106609216+akrrkdgksdywjd@users.noreply.github.com>
2023-02-09 20:24:44 +0900

    홈화면 코틀린 수정

--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
@@ -1,17 +1,12 @@
+
 package com.aiden.tflite.tfliteimageclassifier
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_home.*
 
-class Home : AppCompatActivity() , BottomNavigationView.OnNavigationItemSelectedListener {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
-
+        setContentView(R.layout.activity_history)
     }
 }
