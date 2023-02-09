3251614b4a043fe34cfb2ad2bf6ad13bf89f7175
akrrkdgksdywjd <106609216+akrrkdgksdywjd@users.noreply.github.com>
2023-02-09 20:11:17 +0900

    홈 화면 하반바 수정

--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
@@ -1,11 +1,39 @@
 package com.aiden.tflite.tfliteimageclassifier
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_home.*
 
-class Home : AppCompatActivity() {
+class Home : AppCompatActivity() , BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        bottomNavi.setOnNavigationItemSelectedListener(this)
+
+        supportFragmentManager.beginTransaction().add(R.id.linearLayout, HomeFragment()).commit()
+    }
+
+    override fun onNavigationItemSelected(item: Menu): Boolean {
+
+        when(item.home) {
+            R.id.page_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearLayout , HomeFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.page_tv -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearLayout, TVFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.page_calendar -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearLayout, CalendarFragment()).commitAllowingStateLoss()
+                return true
+            }
+        }
+
+        return false
     }
 }
