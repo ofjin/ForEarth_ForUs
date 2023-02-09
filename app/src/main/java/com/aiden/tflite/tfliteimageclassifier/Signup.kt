3ca1724b26c9eaee215b4774e37e2c87e3056eed
akrrkdgksdywjd <106609216+akrrkdgksdywjd@users.noreply.github.com>
2023-02-09 16:43:49 +0900

    로그인&회원가입 기능

--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Signup.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Signup.kt
@@ -2,10 +2,54 @@ package com.aiden.tflite.tfliteimageclassifier
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class Signup : AppCompatActivity() {
+    private var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        auth = Firebase.auth
+
+//로그인 화면으로 돌아가기
+        back_myinfo.setOnClickListener{
+            startActivity(Intent(this,SigninActivity::class.java))
+        }
+
+
+// 계정 생성 버튼
+        btnSignUpNext.setOnClickListener{
+            createAccount(edtSignUpID.text.toString(),edtSignUpPwd.text.toString())
+        }
     }
-}
+
+    // 계정 생성
+    private fun createAccount(email: String, password: String) {
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth?.createUserWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this, "계정 생성 완료.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish() // 가입창 종료
+                    } else {
+                        Toast.makeText(
+                            this, "계정 생성 실패",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }}
+
+
+
+
