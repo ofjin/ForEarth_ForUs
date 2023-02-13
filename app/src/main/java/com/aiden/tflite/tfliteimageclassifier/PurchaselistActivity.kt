887a7fed518b83657cd1ef1b8310c654467a8c47
wlsskdbwls <wlsskdbwls@naver.com>
2023-02-13 17:56:40 +0900

    구매내역 확인버튼

--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/PurchaselistActivity.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/PurchaselistActivity.kt
@@ -3,11 +3,15 @@ package com.aiden.tflite.tfliteimageclassifier
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class PurchaselistActivity : AppCompatActivity() {
+    //사용 확인 버튼 선언
+    private lateinit var btn_confirm: Button
+    //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_purchaselist)
@@ -22,5 +26,15 @@ class PurchaselistActivity : AppCompatActivity() {
         rv_list.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         rv_list.setHasFixedSize(true)
         rv_list.adapter=profileAdaptor(profileList)
+
+        btn_confirm.setOnClickListener {
+            // 다이얼로그를 생성하기 위해 Builder 클래스 생성자를 이용해 줍니다.
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("확인 처리를 하시겠습니까?")
+                .setMessage("메세지 내용 부분 입니다.")
+            // 다이얼로그를 띄워주기
+            builder.show()
+
+        }
     }
 }
