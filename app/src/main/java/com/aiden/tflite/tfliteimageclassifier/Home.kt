06cde57c12392b37788e507401ff814b719eb257
akrrkdgksdywjd <106609216+akrrkdgksdywjd@users.noreply.github.com>
2023-02-13 16:48:10 +0900

    파이어베이스 로그인 기능 json 파일 수정

--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
@@ -4,7 +4,6 @@ package com.aiden.tflite.tfliteimageclassifier
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_home_test.*
 
 
@@ -12,6 +11,11 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_test)
+
+        //히스토리 버튼
+        history_button.setOnClickListener{
+            startActivity(Intent(this,history::class.java))
+        }
 
         //네비게이션  바
         home.setOnClickListener{
