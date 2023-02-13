05a258804da3340af262c4ae3d9d2c9f485fa2bd
ofjin <90182447+ofjin@users.noreply.github.com>
2023-02-13 16:11:48 +0900

    activity_home.xml 삭제

--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Home.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
-import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_home_test.*
 
 
