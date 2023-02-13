6a3b778665f9ea4c6f13bea8fda0510553198070
wlsskdbwls <wlsskdbwls@naver.com>
2023-02-13 17:16:38 +0900

    제이슨 파일 변경

--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/PurchaselistActivity.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/PurchaselistActivity.kt
@@ -2,8 +2,13 @@ package com.aiden.tflite.tfliteimageclassifier
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PurchaselistActivity : AppCompatActivity() {
+
+    //사용 확인 버튼 선언
+    private lateinit var btn_confirm: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_purchaselist)
