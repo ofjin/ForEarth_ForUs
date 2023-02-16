bbda3c767a2f9686f529cdee52e43e774f74fc71
ofjin <90182447+ofjin@users.noreply.github.com>
2023-02-16 16:11:17 +0900

    버튼 연결

--- app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Stamp.kt
+++ app/src/main/java/com/aiden/tflite/tfliteimageclassifier/Stamp.kt
@@ -1,11 +1,22 @@
 package com.aiden.tflite.tfliteimageclassifier
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Stamp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stamp)
+
+        // 히스토리 버튼 클릭 시
+        val historyBtn = findViewById<ImageView>(R.id.history_btn)
+
+        historyBtn.setOnClickListener {
+            val intent = Intent(this, history::class.java)
+            startActivity(intent)
+        }
+
     }
 }
